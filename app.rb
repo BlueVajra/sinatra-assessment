5ba83179190734c30fd5907f85a384f992ea6ebd
BlueVajra <cory.leistikow@gmail.com>
2014-03-31 09:42:20 -0600

    User can see added product on home page, refresh does not re-submit form

--- app.rb
+++ app.rb
@@ -2,12 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Application
 
+  ITEM_REPOSITORY = []
+
   get '/' do
-    erb :index
+    erb :index, locals: {:items => ITEM_REPOSITORY}
   end
 
   get '/items/new' do
     erb :new_product
+  end
+
+  post '/items' do
+    ITEM_REPOSITORY << params['new_product']
+    redirect('/')
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,14 +5,15 @@ require_relative '../app'
 Capybara.app = App
 
 feature "Item management" do
-  scenario "User can click the link 'Add a Product'" do
+  scenario "User can add a product and see it on the home page" do
 
     visit '/'
     expect(page).to have_content("Welcome")
+    expect(page).to_not have_content("Cory's Awesome Product")
     click_on("Add a Product")
     fill_in "new_product", with: "Cory's Awesome Product"
     click_on("Create Product")
-
+    expect(page).to have_content("Cory's Awesome Product")
 
   end
 end
